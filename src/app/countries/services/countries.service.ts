a56e099fcc4589c1fda63dfd9f94618141aac1bd
Jose Vasquez <vdjoseluis@outlook.com>
2024-11-15 13:39:31 +0100

    Add country information page

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +9,14 @@ export class CountriesService {
   constructor(private http: HttpClient) { }
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  searchCountryByCode(code: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
+    .pipe(
+      map(countries => countries.length > 0 ? countries[0] : null),
+      catchError(() => of(null)),
+    );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
     return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`).pipe(
