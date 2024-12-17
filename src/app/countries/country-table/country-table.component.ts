840896a2c527427c054009668167853b61d6080a
Jose Vasquez <vdjoseluis@outlook.com>
2024-12-17 12:40:53 +0100

    Create filesystem, components, interfaces and pages

---
+++ src/app/countries/country-table/country-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-table',
+  imports: [],
+  templateUrl: './country-table.component.html',
+  styles: ``
+})
+export class CountryTableComponent {
+
+}
